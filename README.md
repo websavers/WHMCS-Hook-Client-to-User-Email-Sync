05730f4eeb2277c95050429085cad1315b69a9d8
Jordan Schelew <jordan@websavers.ca>
2025-06-13 16:12:55 -0300

    Verify the logged in user is the owner before allowing the email change
    This should prevent a very unlikely, but possible, edge case of a malicious user changing the account info ultimately changing the owner user login email.

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ Details in this feature request: https://requests.whmcs.com/idea/updating-the-cl
 
 # How to use
 Upload the PHP file to your HWMCS installations includes/hooks folder and nothing more is required - it will automatically ensure that when a user email is changed, the matching account email is also changed and vice versa. It will only change the 'other' email address if they were originally matching between the user and account.
+
+# Security considerations
+We have to assume WHMCS is only firing these hooks when the user making the changes has permissions to do so.
+
+When doing client -> user email push, the logged in user could be a secondary user on the account that is malicious, with permissions to update the account profile info. To mitigate this, the code now validates that the current user logged in is the same as the user being updated and that they're the owner before making the email address change. This prevents secondary users from changing the owner user email address via the account.
--- client_user_email_sync.php
+++ client_user_email_sync.php
@@ -3,6 +3,9 @@ if (!defined('WHMCS')) {
         exit('This file cannot be accessed directly');
 }
 
+use WHMCS\Authentication\CurrentUser;
+use WHMCS\Session;
+
 add_hook('UserEdit', 5, 'sync_user_to_client');
 add_hook('ClientEdit', 5, 'sync_client_to_user');
 
@@ -31,16 +34,29 @@ function sync_client_to_user($vars) {
 
     // check to see if email address updated
     if ($newmail != $oldmail) {
-        // find the user account
-        $api_getusers = localAPI('GetUsers', array('search' => $oldmail));
-        $user_id = $api_getusers['users'][0]['id'];
-        $api_updateuser = localAPI('UpdateUser', array(
-            'user_id' => $user_id,
-            'email' => $newmail,
-        ));
-        if ($api_updateuser['result']=="success") {
-            logActivity("Sync Client to User Update - User ID  $user_id updated email from $oldmail to $newmail");
+
+        // Ensure the user logged in is the owner of the account
+        $user = CurrentUser::user();
+        $client = CurrentUser::client();
+        if ($user && $client && $user->isOwner($client)) {
+
+            // find the user account
+            $api_getusers = localAPI('GetUsers', array('search' => $oldmail));
+            $user_id = $api_getusers['users'][0]['id'];
+
+            if ($user_id == Session::get('uid')){
+                $api_updateuser = localAPI('UpdateUser', array(
+                    'user_id' => $user_id,
+                    'email' => $newmail,
+                ));
+                if ($api_updateuser['result']=="success") {
+                    logActivity("Sync Client to User Update - User ID  $user_id updated email from $oldmail to $newmail");
+                }
+            }
         }
+    
     }
+
+
 
 }
